cc89b9045ab92307d3eeaca13bf1c23cbb203d8d
“markelld” <markelldehaney@gmail.com>
2021-09-17 14:12:10 -0500

    creating image gallery using grid

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,13 +3,24 @@ import React from "react";
 const Home = () => {
   
   return (
-
-    <div>
-      
-    </div>
+      <div className="p-4 grid-place-items-center min-h-screen bg-purple-500 ">
+        <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <h1>Markell Dehaney</h1>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div> 
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+          <div className="bg-white rounded-md shadow-md h-16"></div>
+        </div>
+      </div>
   )
 }
 
 export default Home; 
+
+
 
 

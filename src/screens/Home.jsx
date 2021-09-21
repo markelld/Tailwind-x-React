5750d13c7374ad1f92eaad84e12d21f17924193c
“markelld” <markelldehaney@gmail.com>
2021-09-21 10:07:06 -0500

    layout for home page in order

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,23 +3,44 @@ import React from "react";
 const Home = () => {
   
   return (
-      <div className="p-4 grid-place-items-center min-h-screen bg-purple-500 ">
-        <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <h1>Markell Dehaney</h1>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div> 
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
-          <div className="bg-white rounded-md shadow-md h-16"></div>
+    // <div className="flex justify-center min-h-screen bg-purple-500 ">
+    //   <div className="min-w-full min-h-screen grid-place-items-center">
+    //     <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    //       <h1>Markell Dehaney</h1>
+    //       <div className="bg-white rounded-md shadow-md h-32 w-32"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div> 
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //       <div className="bg-white rounded-md shadow-md h-16"></div>
+    //     </div>
+    //   </div>
+    // </div>  
+    <div className=" flex justify-center min-h-full bg-purple-500">
+      <div className=" min-h-full grid-place-items-center">
+        <div className="p-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div>
+            //link to instagram
+          </div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
         </div>
       </div>
+    </div>
   )
 }
 
 export default Home; 
+      
+
 
 
 

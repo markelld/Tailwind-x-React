12d37100280cfda7421b7989ff873d4ce06cdfac
“markelld” <markelldehaney@gmail.com>
2021-09-22 09:57:53 -0500

    ui edits

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="h-32" style={{background:"#0F1108"}}>
 
     </div>
   )
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,29 +1,24 @@
 import React from "react";
+import{ Link } from "react-router-dom";
+
 
 const Home = () => {
   
   return (
-    // <div className="flex justify-center min-h-screen bg-purple-500 ">
-    //   <div className="min-w-full min-h-screen grid-place-items-center">
-    //     <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    //       <h1>Markell Dehaney</h1>
-    //       <div className="bg-white rounded-md shadow-md h-32 w-32"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div> 
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //       <div className="bg-white rounded-md shadow-md h-16"></div>
-    //     </div>
-    //   </div>
-    // </div>  
     <div className=" flex justify-center min-h-full bg-purple-500">
       <div className=" min-h-full grid-place-items-center">
         <div className="p-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <div>
-            //link to instagram
+          <div className="">
+            //link to instagram  
+            <Link>
+              <h1 className="p-1">@markelldehaney</h1>
+            </Link>
+            <Link>
+              <h1 className="p-1">https://markelldehaney.netlify.app</h1>
+            </Link>
           </div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
+          <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
           <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
           <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
           <div className="bg-white rounded-md shadow-md h-64 w-64"></div>
@@ -39,6 +34,7 @@ const Home = () => {
 }
 
 export default Home; 
+
       
 
 
